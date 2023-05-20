11da5d5ed8da04a1ec0607c9ab1c1e1e0e4f7d71
Shubham Talbar <shubham.talbar@appfolio.com>
2023-05-19 17:33:09 -0700

    Add basic authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "shubham", password: "secret",  only: :destroy
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
